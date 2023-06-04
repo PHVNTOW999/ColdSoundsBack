38f2179260aa7cdee9e71cab9e4859f8a3014022
Ali Tim <ali.tim03.at@gmail.com>
2023-06-04 09:12:23 +0600

    update

--- main/models.py
+++ main/models.py
@@ -3,8 +3,8 @@ import uuid
 from django.core.validators import FileExtensionValidator
 from django.db import models
 
-class Artist(models.Model):
 
+class Artist(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid1(), editable=False)
 
     name = models.CharField(
@@ -13,7 +13,7 @@ class Artist(models.Model):
         blank=False,
         verbose_name="Name"
     )
-    
+
     avatar = models.ImageField(
         null=True,
         blank=True,
@@ -21,49 +21,8 @@ class Artist(models.Model):
     )
 
     class Meta:
-        verbose_name='Artist'
-        verbose_name_plural='Artists'
-
-    def __str__(self):
-        return self.name
-
-class Album(models.Model):
-
-    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
-
-    name = models.CharField(
-        max_length=155,
-        null=False,
-        blank=False,
-        verbose_name="Name"
-    )
-
-    artists = models.ManyToManyField(
-        Artist,
-        null=False,
-        blank=False,
-        verbose_name="Artists",
-        related_name="album_artists"
-    )
-
-    feat = models.ManyToManyField(
-        Artist,
-        null=True,
-        blank=True,
-        verbose_name="Featuring Artists"
-    )
-    
-    cover = models.ImageField(
-        null=True,
-        blank=True,
-        verbose_name="Cover"
-    )
-
-    date = models.DateField()
-
-    class Meta:
-        verbose_name='Album'
-        verbose_name_plural='Albums'
+        verbose_name = 'Artist'
+        verbose_name_plural = 'Artists'
 
     def __str__(self):
         return self.name
@@ -72,17 +31,18 @@ class Album(models.Model):
 class Single(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
 
+    cover = models.ImageField(
+        validators=[FileExtensionValidator(['png', 'jpg'])],
+        upload_to='img',
+        blank=True,
+        verbose_name='Single Cover',
+    )
+
     name = models.CharField(
         max_length=155,
         null=False,
         blank=False,
         verbose_name="Name"
-    )
-
-    file = models.FileField(
-        validators=[FileExtensionValidator(['mp3', 'wav'])],
-        upload_to='audio',
-        verbose_name='Single File'
     )
 
     artists = models.ManyToManyField(
@@ -100,11 +60,10 @@ class Single(models.Model):
         verbose_name="Featuring Artists"
     )
 
-    cover = models.ImageField(
-        validators=[FileExtensionValidator(['png', 'jpg'])],
-        upload_to='img',
-        blank=True,
-        verbose_name='Single Cover',
+    file = models.FileField(
+        validators=[FileExtensionValidator(['mp3', 'wav'])],
+        upload_to='audio',
+        verbose_name='Audio File'
     )
 
     date = models.DateField()
@@ -112,6 +71,55 @@ class Single(models.Model):
     class Meta:
         verbose_name = 'Single'
         verbose_name_plural = 'Singles'
+
+    def __str__(self):
+        return self.name
+
+
+class Album(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
+
+    name = models.CharField(
+        max_length=155,
+        null=False,
+        blank=False,
+        verbose_name="Name"
+    )
+
+    cover = models.ImageField(
+        null=True,
+        blank=True,
+        verbose_name="Cover"
+    )
+
+    artists = models.ManyToManyField(
+        Artist,
+        null=False,
+        blank=False,
+        verbose_name="Artists",
+        related_name="album_artists"
+    )
+
+    feat = models.ManyToManyField(
+        Artist,
+        null=True,
+        blank=True,
+        verbose_name="Featuring Artists"
+    )
+
+    files = models.ManyToManyField(
+        Single,
+        null=False,
+        blank=False,
+        verbose_name="Singles",
+        related_name="album_single",
+    )
+
+    date = models.DateField()
+
+    class Meta:
+        verbose_name = 'Album'
+        verbose_name_plural = 'Albums'
 
     def __str__(self):
         return self.name
